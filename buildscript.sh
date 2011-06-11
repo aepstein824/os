1c404646f2d2fe0d5fe93250e6be488892e41ed1
Maxime Serrano <mserrano@duality.(none)>
2011-06-10 20:49:24 -0700

    fix up the build script

--- buildscript.sh
+++ buildscript.sh
@@ -1,4 +1,10 @@
+# clean
+rm -rf *.o
+rm -rf *.bin
+
+nasm -f elf -o loader.o loader.s
 gcc -o kernel.o -c kernel.c -nostdlib -nostartfiles -nodefaultlibs
 gcc -o string.o -c string.c -nostdlib -nostartfiles -nodefaultlibs
-ld -T linker.ld -o kernel.bin loader.o kernel.o string.o
+gcc -o console.o -c console.c -nostdlib -nostartfiles -nodefaultlibs
+ld -T linker.ld -o kernel.bin loader.o kernel.o string.o console.o
 qemu -kernel kernel.bin
